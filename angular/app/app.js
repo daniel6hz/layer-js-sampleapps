f72c5396595be27099e140318a8ed9f315fa46c6
Michael Kantor <michael@layer.com>
2016-04-29 18:38:13 -0700

    Updated announcements UI

--- angular/app/app.js
+++ angular/app/app.js
@@ -4,8 +4,10 @@
 var app = angular.module('ChatApp', [
   'sampleAppControllers',
   'conversationPanelControllers',
+  'conversationListControllers',
   'newConversationPanelControllers',
-  'announcementControllers',
+  'messageListControllers',
+  'messageItemControllers',
   'infinite-scroll',
   'ngRoute'
 ]);
