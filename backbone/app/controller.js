778263f9f69588318bd5ea59f957e50b78e9e3ca
Michael Kantor <michael@layer.com>
2016-05-17 16:44:06 -0700

    Backbone sample now has Identities

--- backbone/app/controller.js
+++ backbone/app/controller.js
@@ -62,6 +62,13 @@ module.exports = function(client) {
   });
 
   /**
+   * Create Identity List Query
+   */
+  var identityQuery = client.createQuery({
+    model: layer.Query.Identity
+  });
+
+  /**
    * Any time a query data changes we should rerender.  Data changes when:
    *
    * * The Query data has loaded from the server
@@ -106,6 +113,10 @@ module.exports = function(client) {
       return !item.isRead;
     });
     Backbone.$('.announcements-button').toggleClass('unread-announcements', unread.length > 0);
+  });
+  identityQuery.on('change', function() {
+    participantView.users = identityQuery.data;
+    participantView.render();
   });
 
   /**
@@ -159,6 +170,7 @@ module.exports = function(client) {
   });
   sendView.on('conversation:create', function(text) {
     // See http://static.layer.com/sdk/docs/#!/api/layer.Conversation
+    debugger;
     var conversation = client.createConversation(newConversation);
     conversation.createMessage(text).send();
     conversation.on('conversations:sent', function() {
