803fecb3497c3761582ab9f163a886ae2a1e2f49
Michael Kantor <michael@layer.com>
2016-05-18 14:36:41 -0700

    Adds support for Identities in typing indicator events

--- react/src/containers/ActiveConversation.js
+++ react/src/containers/ActiveConversation.js
@@ -80,7 +80,6 @@ export default class ActiveConversation extends Component {
           onLoadMoreMessages={loadMoreMessages}/>
 
         <TypingIndicatorContainer
-          users={users}
           conversationId={activeConversationId}/>
 
         <MessageComposer
